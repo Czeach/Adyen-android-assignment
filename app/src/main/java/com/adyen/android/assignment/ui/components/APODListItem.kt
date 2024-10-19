11ff7622e5a59eed0cec264c27367ae7f340cee1
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2024-10-19 01:00:04 +0100

    implemented saving to favorites and pinning to top of latest list, updated unit tests. project refactor: used a shared viewmodel, reordered project folders and cleaned up

---
+++ app/src/main/java/com/adyen/android/assignment/ui/components/APODListItem.kt
@@ -0,0 +1,77 @@
+package com.adyen.android.assignment.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.adyen.android.assignment.R
+
+@Composable
+fun APODListItem(
+    modifier: Modifier,
+    title: String,
+    imageUrl: String,
+    date: String,
+    onAPODClicked: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .clickable {
+                onAPODClicked()
+            }
+    ) {
+        Card(
+            modifier = Modifier
+                .size(50.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = CardDefaults.cardColors(
+            )
+        ) {
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = stringResource(R.string.image_description),
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp)
+        ) {
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(6.dp)
+            )
+            Text(
+                text = date,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/adyen/android/assignment/ui/components/AdditionalInfoRow.kt
@@ -0,0 +1,37 @@
+package com.adyen.android.assignment.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.adyen.android.assignment.R
+
+@Composable
+fun AdditionalInfoRow(iconRes: Int, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp)
+    ) {
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = stringResource(R.string.icon_description),
+            tint = MaterialTheme.colorScheme.secondary,
+        )
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(start = 10.dp)
+        )
+    }
+}
